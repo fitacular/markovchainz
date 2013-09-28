9fc5563c770d3d9fda60603696f2a6b83b2c04b1
Alejandro Ciniglio <mail@alejandrociniglio.com>
2013-09-27 21:32:25 -0400

    initial

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to markovchainz
+
+TODO: write [great documentation](http://jacobian.org/writing/great-documentation/what-to-write/)
---
+++ test/markovchainz/core_test.clj
@@ -0,0 +1,7 @@
+(ns markovchainz.core-test
+  (:require [clojure.test :refer :all]
+            [markovchainz.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
