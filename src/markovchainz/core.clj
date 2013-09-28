e5811e63d3de6746c3e5fa8ca35ae1503212598c
dsri <dkbsri@gmail.com>
2013-09-28 17:42:52 -0400

    print more lines

--- src/markovchainz/core.clj
+++ src/markovchainz/core.clj
@@ -1,14 +1,23 @@
 (ns markovchainz.core
   (:require
+    [clojure.string :as str]
     [markovchainz.text-generator :as text-generator])
   (:use ring.adapter.jetty)
   (:use ring.middleware.reload)
   (:use ring.middleware.stacktrace))
 
+(defn get-stanza []
+  (let [lines 5]
+    (str (str/join "\n" (repeatedly lines #(text-generator/line))) "\n")))
+
+(defn get-body []
+  (let [stanzas 3]
+    (str (str/join "\n" (repeatedly stanzas #(get-stanza))) "\n")))
+
 (defn handler [req]
   {:status  200
    :headers {"Content-Type" "text/plain"}
-   :body    "hello, world!\n"})
+   :body    (get-body)})
 
 (def app
   (-> #'handler
@@ -19,5 +28,4 @@
   (run-jetty #'app {:port 8080}))
 
 (defn -main []
-  (boot)
-  )
+  (boot))
