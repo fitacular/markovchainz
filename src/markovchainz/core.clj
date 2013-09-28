8d56d45e21e9ea081869ec848a81867284ffd951
dsri <dkbsri@gmail.com>
2013-09-28 16:03:16 -0400

    add ring boot

--- src/markovchainz/core.clj
+++ src/markovchainz/core.clj
@@ -1,6 +1,23 @@
-(ns markovchainz.core)
+(ns markovchainz.core
+  (:require
+    [markovchainz.text-generator :as text-generator])
+  (:use ring.adapter.jetty)
+  (:use ring.middleware.reload)
+  (:use ring.middleware.stacktrace))
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(defn handler [req]
+  {:status  200
+   :headers {"Content-Type" "text/plain"}
+   :body    "hello, world!\n"})
+
+(def app
+  (-> #'handler
+    (wrap-reload '(ring-tutorial.core))
+    (wrap-stacktrace)))
+
+(defn boot []
+  (run-jetty #'app {:port 8080}))
+
+(defn -main []
+  (boot)
+  )
